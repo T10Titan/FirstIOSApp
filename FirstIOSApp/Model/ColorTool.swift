8732377f02497fcb5a1e1ff2fdb434e31307629e
Trujillo <ttru4447@740s-pl-129.local>
2018-10-09 08:53:36 -0600

    RGB put in

--- FirstIOSApp/Model/ColorTool.swift
+++ FirstIOSApp/Model/ColorTool.swift
@@ -9,7 +9,14 @@
 import Foundation
 import UIKit
 
-public class Color Tool
+public class ColorTool
 {
-    
+    public func createRandomColor() -> UIColor
+    {
+        let  redPercent : CGFloat = CGFloat ((arc4random() % 256) / 255)
+        let greenPercent : CGFloat = CGFloat ((arc4random() % 256) / 255)
+        let bluePercent : CGFloat = CGFloat ((arc4random() % 256) / 255)
+        
+        return UIColor(red: redPercent, green: greenPercent, blue: bluePercent, alpha: CGFloat(100) )
+    }
 }
