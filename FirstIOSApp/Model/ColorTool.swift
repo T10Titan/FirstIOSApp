26d3d037e5a6d89c9095caa1c9c5da0304760dac
Trujillo <ttru4447@740s-pl-129.local>
2018-10-09 08:44:28 -0600

    Model folder and colortool

---
+++ FirstIOSApp/Model/ColorTool.swift
@@ -0,0 +1,15 @@
+//
+//  ColorTool.swift
+//  FirstIOSApp
+//
+//  Created by Trujillo, Teyton on 10/9/18.
+//  Copyright © 2018 ctec. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class Color Tool
+{
+    
+}
